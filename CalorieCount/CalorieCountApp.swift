5c9b886563193bf13ec7e33b1ca627f65d78f48a
annashi4 <ann.shirinkina@gmail.com>
2022-06-24 00:41:38 +0400

    Initial Commit

---
+++ CalorieCount/CalorieCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieCountApp.swift
+//  CalorieCount
+//
+//  Created by Anna on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalorieCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
