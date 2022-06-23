d8e18d4aff314a7bb78793efaf8df7eb2f8ca2d9
annashi4 <ann.shirinkina@gmail.com>
2022-06-24 02:06:45 +0400

    wrong declaration fixed

--- CalorieCount/Extentions/TimeFormating.swift
+++ CalorieCount/Extentions/TimeFormating.swift
@@ -4,7 +4,7 @@ import Foundation
 func culcTimeSince(date: Date) -> String {
     let minutes = Int(-date.timeIntervalSinceNow)/60
     let hours = minutes/60
-    let dyas = hours/24
+    let days = hours/24
     
     if minutes < 60 {
         return "\(minutes) minutes ago"
