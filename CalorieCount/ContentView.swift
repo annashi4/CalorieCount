272ed7a28fbaa4c0448f5352d9b0fef42fb2e566
annashi4 <ann.shirinkina@gmail.com>
2022-06-24 12:09:27 +0400

    getting data from the data base

--- CalorieCount/ContentView.swift
+++ CalorieCount/ContentView.swift
@@ -1,9 +1,20 @@
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @FetchRequest (sortDescriptors: [SortDescriptor(\.date, order: .reverse)]) var food: FetchedResults<Food>
+    
+    
+    @State private var showingAddView = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack(alignment: .leading){
+//                Text("\(Int(totalCaloriesToday)) Kcal (Today)")
+            }
+            .navigationTitle("Calories Counter")
+        }
     }
 }
 
--- CalorieCount/Extentions/TimeFormating.swift
+++ CalorieCount/Extentions/TimeFormating.swift
@@ -5,8 +5,10 @@ func culcTimeSince(date: Date) -> String {
     let minutes = Int(-date.timeIntervalSinceNow)/60
     let hours = minutes/60
     let days = hours/24
-    
-    if minutes < 60 {
+     
+    if minutes == 1 {
+        return ("1 minutie ago")
+    }else if minutes < 60 {
         return "\(minutes) minutes ago"
     } else if minutes >= 60 && hours < 24 {
         return ("\(hours) hours ago")
